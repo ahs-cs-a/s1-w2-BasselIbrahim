c107c6b4176b594e3d12da804b6edd00a8555a12
BasselIbrahim <basselki26@ausdk12.org>
2024-08-27 19:18:36 -0700

    Update BodyOfWaterMain.java

--- BodyOfWaterMain.java
+++ BodyOfWaterMain.java
@@ -2,16 +2,17 @@ public class BodyOfWaterMain {
     public static void main(String[] args) {
         BodyOfWater b = new BodyOfWater("Ocean", 25, 100, true);
         BodyOfWater b1 = new BodyOfWater("Lake", 30);
-        BodyOfWater b2 = new BodyOfWater("CoolWater", 25, 110, true);
+        //BodyOfWater b2 = new BodyOfWater("CoolWater", 25, 110, true);
 
         
-        System.out.println(b.name());
-        System.out.println(b2.name());
-        System.out.println(b.largestDiameter());
-        System.out.println(b.avgDepth());
+        System.out.println(b.getName());
+        //System.out.println(b2.name());
+        System.out.println(b.getLargestDiameter());
+        System.out.println(b.getAvgDepth());
         System.out.println(b.isSaltWater());
-        System.out.println(b1.name());
-        System.out.println(b1.avgDepth());
-
+        System.out.println(b1.getName());
+        System.out.println(b1.getAvgDepth());
+        b1.setName("Lake Anza") 
+        System.out.println(b1.getName());
     }
 }
